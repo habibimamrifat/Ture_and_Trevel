05688f8f22c4adc67db6c457d1f3c5c288a9beff
habibimamrifat <habibrifatx21@gmail.com>
2024-03-01 18:42:04 +0600

    sign up and sign in done

--- src/component/BaMiniMenu.jsx
+++ src/component/BaMiniMenu.jsx
@@ -1,156 +1,198 @@
 import React, { useState } from "react";
 import flag1 from "../assets/img/Ellipse 30.png";
 import { FaAngleDown } from "react-icons/fa";
+import SignUpPage from "./SignInPage";
+import SignIn from "./SignUp";
 
 const BaMiniMenu = () => {
-
   let [setCountry, viewCuntryOption] = useState(false);
-  let [destination, setDestination]= useState('BAN')
-  let [flag, setFlag] = useState(flag1)
-  
+  let [destination, setDestination] = useState("BAN");
+  let [flag, setFlag] = useState(flag1);
 
-  let countryAndFlagArr =[
+  let [isSignupClosed, setIsSignUpClosed] = useState(null);
+
+  let countryAndFlagArr = [
     {
-        id:"c1",
-        country:"Ban",
-        flag:flag1
+      id: "c1",
+      country: "Ban",
+      flag: flag1,
     },
     {
-        id:"c2",
-        country:"ENG",
-        flag:flag1
+      id: "c2",
+      country: "ENG",
+      flag: flag1,
     },
     {
-        id:"c3",
-        country:"CHI",
-        flag:flag1
+      id: "c3",
+      country: "CHI",
+      flag: flag1,
     },
     {
-        id:"c4",
-        country:"ITAI",
-        flag:flag1
-    }
-  ]
-  let InitialOptionCountry = countryAndFlagArr.filter((countryandflag)=>countryandflag.id != "c1")
-  let [ViewOptionCountry, setViewOPtionCountry]=useState(InitialOptionCountry)
+      id: "c4",
+      country: "ITAI",
+      flag: flag1,
+    },
+  ];
+  let InitialOptionCountry = countryAndFlagArr.filter(
+    (countryandflag) => countryandflag.id != "c1"
+  );
+  let [ViewOptionCountry, setViewOPtionCountry] =
+    useState(InitialOptionCountry);
 
-
-// function to handle currenct of the select box
+  // function to handle currenct of the select box
   function handleCurency(currency) {
     console.log(currency);
   }
 
-//   function to handle country and country flag of the select box
+  //   function to handle country and country flag of the select box
 
-  function setCountryAndFlag( id )
-  {
+  function setCountryAndFlag(id) {
+    let CountryAndFlagSearch = countryAndFlagArr.find(
+      (countryandflag) => countryandflag.id == id
+    );
+    setDestination((destination = CountryAndFlagSearch.country));
+    setFlag((flag = CountryAndFlagSearch.flag));
 
-    let CountryAndFlagSearch = countryAndFlagArr.find((countryandflag) => countryandflag.id == id)
-    setDestination(destination = CountryAndFlagSearch.country);
-    setFlag(flag = CountryAndFlagSearch.flag);
-
-    let OptionCountry = countryAndFlagArr.filter((countryandflag)=>countryandflag.id != id)
+    let OptionCountry = countryAndFlagArr.filter(
+      (countryandflag) => countryandflag.id != id
+    );
     console.log(OptionCountry);
-    setViewOPtionCountry(ViewOptionCountry = OptionCountry)
-    viewCuntryOption(!setCountry)
-
+    setViewOPtionCountry((ViewOptionCountry = OptionCountry));
+    viewCuntryOption(!setCountry);
   }
- 
+
+  // this is for sign up function
+ const setSignupPeramiter =(signupparamiter)=>{
+  console.log("signup paramiter", signupparamiter)
+  
+  if(isSignupClosed === signupparamiter)
+  {
+    
+    setIsSignUpClosed(null)
+    
+  }
+  else
+  {
+    console.log("not equal")
+    setIsSignUpClosed(signupparamiter)
+    
+  }
+ }
 
   return (
     <div className="pt-[83Px] flex justify-between items-center ">
-
       <div className="text-white font-bold text-base flex justify-center items-center pl-[315px]">
+        <div className="mr-[21px]">
+          <h1>Support</h1>
+        </div>
 
-            <div className="mr-[21px]">
-            <h1>Support</h1>
-            </div>
+        <div className="flex  mr-[25px]">
+          <select
+            name=""
+            id=""
+            className="bg-transparent border-none"
+            onChange={(e) => handleCurency(e.target.value)}
+          >
+            <option value="EUR" className="text-black">
+              EURO
+            </option>
+            <option value="GBP" className="text-black">
+              GBP
+            </option>
+            <option value="RUB" className="text-black">
+              RUB
+            </option>
+          </select>
+        </div>
 
-            <div className="flex  mr-[25px]">
-            <select
-                name=""
-                id=""
-                className="bg-transparent border-none"
-                onChange={(e) => handleCurency(e.target.value)}
+        <div className="relative">
+          <div className="flex justify-center items-center gap-2">
+            <div
+              className="flex justify-center items-center gap-5"
+              onClick={() => viewCuntryOption(!setCountry)}
             >
-                <option value="EUR" className="text-black">
-                EURO
-                </option>
-                <option value="GBP" className="text-black">
-                GBP
-                </option>
-                <option value="RUB" className="text-black">
-                RUB
-                </option>
-            </select>
+              <div className="h-14px w-15px rounded-full">
+                <img
+                  src={flag}
+                  className="object-cover overflow-hidden"
+                  alt=""
+                />
+              </div>
+              <div className="flex justify-center items-center">
+                <h1 className="text-white">{destination}</h1>
+                <FaAngleDown />
+              </div>
             </div>
+          </div>
 
-            <div className="relative">
-
-                <div className="flex justify-center items-center gap-2">
-                    <div className="flex justify-center items-center gap-5"
-                    onClick={() => viewCuntryOption(!setCountry)}
-                    >
-                        <div className="h-14px w-15px rounded-full">
-                            <img
-                            src={flag}
-                            className="object-cover overflow-hidden"
-                            alt=""
-                            />
-                        </div>
-                        <div className="flex justify-center items-center">
-                            <h1 className="text-white">{destination}</h1>
-                            <FaAngleDown />
-                        </div>
-                    </div>
+          <div
+            className={`${
+              setCountry
+                ? "block  text-black absolute right-1 top-5 h-[73px] w-[90px] bg-white rounded-md"
+                : "hidden"
+            }`}
+          >
+            {ViewOptionCountry.map((option) => (
+              <div
+                className="flex justify-center items-center gap-5"
+                id={option.id}
+                onClick={(e) => setCountryAndFlag(e.currentTarget.id)}
+                key={option.id}
+              >
+                <div className="h-14px w-15px rounded-full">
+                  <img
+                    src={option.flag}
+                    className="object-cover overflow-hidden"
+                    alt=""
+                  />
                 </div>
-
-                <div className={`${setCountry ? 'block  text-black absolute right-1 top-5 h-[73px] w-[90px] bg-white rounded-md' : 'hidden'}`}>
-
-                    {
-                        ViewOptionCountry.map((option)=>(
-                            <div className="flex justify-center items-center gap-5" id={option.id} onClick={(e)=>setCountryAndFlag(e.currentTarget.id)} key={option.id}>
-                            <div className="h-14px w-15px rounded-full">
-                                <img
-                                src={option.flag}
-                                className="object-cover overflow-hidden"
-                                alt=""
-                                />
-                            </div>
-                            <div className="flex justify-center items-center">
-                                <h1 className=" text-[15px] font-normal">{option.country}</h1>
-                            
-                            </div>
-                        </div> 
-                       ))
-                    }
-                    
+                <div className="flex justify-center items-center">
+                  <h1 className=" text-[15px] font-normal">{option.country}</h1>
                 </div>
-            </div>
-
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
-      
-
       <div className="text-white font-bold text-base flex gap-1 pr-[108px]">
-        <button>
+        <button onClick={() => setSignupPeramiter("Signup")}>
           <h1>Sign Up</h1>
         </button>
 
         <h1>/</h1>
 
-        <button>
-          <h1>
-          Sign In
-          </h1>
-          </button>
+        <button onClick={() => setSignupPeramiter("Signin")}>
+          <h1>Sign In</h1>
+        </button>
 
-          <div className=" bg-white/20 absolute top-0 bottom-0 left-0 right-0">
-            <div className="h-[600px] w-96 bg-red-950 absolute right-0">
+        <div className={`absolute bg-white ease-in-out  ${ isSignupClosed === 'Signup' || isSignupClosed === "Signin" ? ' top-0 right-0 ' : 'delay-500 -top-[99999px] -right-[99999px]' } `}>
 
+
+          <div
+            className={`bg-white absolute right-0 shadow-md shadow-gray-400 z-[100] transition-min-height transition-min-w duration-1000 ${
+              isSignupClosed === 'Signup' || isSignupClosed === "Signin" ? "min-h-[964px] min-w-[550px] " : "min-w-0 min-h-0"
+            }`}
+          >
+
+           <button className={`w-[50px] h-[50px] rounded-full flex justify-center items-center bg-black/20 absolute right-5 top-2 ${ isSignupClosed === 'Signup' || isSignupClosed === "Signin" ?' block':'hidden'} `} onClick={() => setSignupPeramiter(null)}>
+
+                <h1 className="text-[12px]" >X</h1>
+            </button>
+
+            <div className={`mt-[80px] ms-[20px] me-[40px] transition-all duration-500 ease-in-out overflow-hidden ${isSignupClosed === 'Signup' || isSignupClosed === "Signin" ?'max-h-full max-w-full':'max-h-0 max-w-0'}`}>
+            
+            {
+               isSignupClosed === "Signup" ? <SignUpPage isSignupClosed={isSignupClosed} setIsSignUpClosed={setIsSignUpClosed }/> : isSignupClosed === "Signin" ? <SignIn /> : null
+            }
+              
             </div>
 
+
           </div>
+
+
+        </div>
       </div>
     </div>
   );
---
+++ src/component/SignInPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const SignUpPage = ({isSignupClosed , setIsSignUpClosed}) => {
+    console.log("from sign up", isSignupClosed)
+  return (
+    <div>
+      
+                <div className="flex justify-between">
+                <h1 className="font-medium text-[36px] text-black">
+                Sign Up
+                </h1>
+                <button>
+                <h1 className="font-normal text-[15px] text-[#FF3B00]" onClick={()=>setIsSignUpClosed("Signin")}>Sign In</h1>
+                </button>
+                </div>
+              
+                <p className="font-normal text-[15px] text-[#B6A7A7] mt-[20px]">Fill in the registration form and save your favorite tours, synchronize them on all devices</p>
+
+                <div className="mt-[40px]">
+                  <h1 className="text-[15px] font-normal text-black">Email address</h1>
+                  <input type="text" className="h-[65px] rounded-[10px] bg-[#F1EAEA] mt-[12px] w-[390px]" />
+                  <h1 className="text-[15px] font-normal text-black mt-[25px]">Email address</h1>
+                  <input type="text" className="h-[65px] rounded-[10px] bg-[#F1EAEA] mt-[12px] w-[390px]" />
+                  <p className="font-normal text-[15px] text-[#B6A7A7] mt-[20px]">Several special characters are allowed, including space, period (.), hyphen (-), apostrophe ('), underscore (_), and the @ sign.</p>
+
+                  <button className="h-[65px] rounded-[10px] bg-[#FF3B00] mt-[12px] w-[390px] text-white flex justify-center items-center">
+                    <h1 className="text-[20px] font-semibold">Create a Account</h1>
+                  </button>
+
+                </div>
+
+                
+    </div>
+  )
+}
+
+export default SignUpPage;
