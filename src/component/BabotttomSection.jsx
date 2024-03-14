dd06fbcfdbd7962fb857d88e1b38023bedded01a
habibimamrifat <habibrifatx21@gmail.com>
2024-03-14 16:08:16 +0600

    working on date picker

--- src/component/BabotttomSection.jsx
+++ src/component/BabotttomSection.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { CiLocationOn } from "react-icons/ci";
 import { FaRegUser } from "react-icons/fa6";
-
 import { LuUserCheck, LuUserX } from "react-icons/lu";
-
 import { SlOptionsVertical } from "react-icons/sl";
 import { RxCrossCircled } from "react-icons/rx";
 import SectionWrapper from "./wrappers/SectionWrapper";
@@ -14,6 +12,9 @@ import { IoCarSport } from "react-icons/io5";
 import { LuPackageOpen } from "react-icons/lu";
 import { GiCruiser } from "react-icons/gi";
 import { MdOutlineHolidayVillage } from "react-icons/md";
+// date peaker section import and css
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import {
   CountryDropdown,
@@ -23,63 +24,67 @@ import {
 
 const BabotttomSection = () => {
   const [toggle, setToggleValue] = useState(false);
+
+  // country dropdown state manegment
   const [country, setCountry] = useState("Bangladesh");
+
+  // date manegment state is maneged here
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  // new Date("mm/dd/yy")
+  // new Date("mm/dd/yy")
+
+  // travelers count
+  const [traveler, setTraveler] = useState(1);
+  const totalPossibletraveler = (condition) => {
+    if (condition === "plus") {
+      console.log("increasing trv");
+      setTraveler(traveler + 1);
+    } else {
+      if (traveler - 1 > 0) {
+        setTraveler(traveler - 1);
+      } else {
+        alert("must be one person trevelling");
+      }
+    }
+  };
+
+  // hotel booking handelling
+  const handleHotelBooking = (event) => {
+    event.preventDefault();
+    const ShowBooking = `country:${country}startingdate:${startDate}endingDtate:${endDate}total treveler:${traveler}`;
+    alert(ShowBooking);
+  };
 
   return (
     <SectionWrapper>
       <div className="flex justify-center items-center">
         <div className="w-[95%] h-auto xl:w-[1047px] xl:h-[225px]  rounded-3xl overflow-hidden shadow-md shadow-slate-500 bg-white">
           {/* ..............................  */}
+          {/* top starts here   */}
 
-          <div className="relative">
-            {/* ........... button to open.... */}
-            <div
-              className={` xsm:absolute sm:hidden text-black ${
-                toggle
-                  ? "xsm:top-[99999px] xsm:left-[99999px]"
-                  : "xsm:top-5 xsm:left-5"
-              }`}
-              onClick={() => setToggleValue(!toggle)}
-            >
-              <SlOptionsVertical className="text-[20px]" />
-            </div>
-            {/* ..........buttton to open ...  */}
-            <div
-              className={`xsm:absolute text-orange-600   sm:h-[45px] w-[100%] sm:relative sm:flex sm:justify-center ${
-                toggle
-                  ? "flex flex-col z-10 rounded-xl bg-white shadow-md sm:flex-row "
-                  : "xsm:top-[99999px] xsm:left-[99999px] sm:top-0 sm:left-0 "
-              }`}
-            >
-              {/* ...........the button to close     */}
-              <div
-                className={` xsm:absolute sm:hidden ${
-                  toggle
-                    ? "xsm:top-5 xsm:right-5 "
-                    : "xsm:top-[99999px] xsm:left-[99999px]"
-                }`}
-                onClick={() => setToggleValue(!toggle)}
-              >
-                <RxCrossCircled className="text-[20px]" />
-              </div>
-              {/* ......the button to close....  */}
+          <div className=" flex text-orange-600 xsm:flex-col md:flex-row">
 
+            <div className="flex flex-1">
               <Link to="#" className="flex-1 border-black/20">
-                <div className="border-[1px]  flex justify-center items-center h-full xsm:w-[80%] sm:w-full sm:flex-1 hover:text-white hover:bg-orange-600 ">
-                  <div className="flex items-center gap-4">
-                    <FaHotel className="font-normal xsm:text-[15px] sm:text-[20px] xsm:hidden lg:block" />
-                    <h1 className="font-normal xsm:text-[15px] sm:text-[20px]">
+                <div className="border-[1px]  flex justify-center items-center h-full w-full flex-1 hover:text-white hover:bg-orange-600 ">
+                  <div className="flex items-center lg:gap-4 xsm:gap-2 xsm:p-[5%] sm:m-0 xsm:flex-col sm:flex-row">
+
+                    <FaHotel className="font-normal  text-[20px]" />
+                    <h1 className="font-normal  text-[20px]">
                       Hotel
                     </h1>
+
                   </div>
                 </div>
               </Link>
 
               <Link to="#" className="flex-1 border-black/20">
-                <div className="border-[1px]  flex justify-center items-center h-full xsm:w-[80%] sm:w-full sm:flex-1 hover:text-white hover:bg-orange-600 ">
-                  <div className="flex items-center gap-4">
-                    <MdFlight className="font-normal xsm:text-[15px] sm:text-[25px] xsm:hidden lg:block" />
-                    <h1 className="font-normal xsm:text-[15px] sm:text-[20px]">
+                <div className="border-[1px]  flex justify-center items-center h-full w-full flex-1 hover:text-white hover:bg-orange-600 ">
+                  <div className="flex items-center lg:gap-4 xsm:gap-2 xsm:p-[5%] sm:m-0 xsm:flex-col sm:flex-row">
+
+                    <MdFlight className="font-normal  text-[30px]" />
+                    <h1 className="font-normal  text-[20px]">
                       Flights
                     </h1>
                   </div>
@@ -87,21 +92,23 @@ const BabotttomSection = () => {
               </Link>
 
               <Link to="#" className="flex-1 border-black/20">
-                <div className="border-[1px]  flex justify-center items-center h-full xsm:w-[80%] sm:w-full sm:flex-1 hover:text-white hover:bg-orange-600 ">
-                  <div className="flex items-center gap-4">
-                    <IoCarSport className="font-normal xsm:text-[15px] sm:text-[25px] xsm:hidden lg:block" />
-                    <h1 className="font-normal xsm:text-[15px] sm:text-[20px]">
+                <div className="border-[1px]  flex justify-center items-center h-full w-full flex-1 hover:text-white hover:bg-orange-600 ">
+                  <div className="flex items-center lg:gap-4 xsm:gap-2 xsm:p-[5%] sm:m-0 xsm:flex-col sm:flex-row">
+                    <IoCarSport className="font-normal  text-[30px]" />
+                    <h1 className="font-normal  text-[20px]">
                       Cars
                     </h1>
                   </div>
                 </div>
               </Link>
+            </div>
 
-              <Link to="/pakagedetails" className="flex-1 border-black/20">
-                <div className="border-[1px]  flex justify-center items-center h-full xsm:w-[80%] sm:w-full sm:flex-1 hover:text-white hover:bg-orange-600 ">
-                  <div className="flex items-center gap-4">
-                    <LuPackageOpen className="font-normal xsm:text-[15px] sm:text-[25px] xsm:hidden lg:block" />
-                    <h1 className="font-normal xsm:text-[15px] sm:text-[20px]">
+            <div className="flex flex-1">
+              <Link to="#" className="flex-1 border-black/20">
+                <div className="border-[1px]  flex justify-center items-center h-full w-full flex-1 hover:text-white hover:bg-orange-600 ">
+                  <div className="flex items-center lg:gap-4 xsm:gap-2 xsm:p-[5%] sm:m-0 xsm:flex-col sm:flex-row">
+                    <LuPackageOpen className="font-normal  text-[25px]" />
+                    <h1 className="font-normal  text-[20px]">
                       Packege
                     </h1>
                   </div>
@@ -109,10 +116,10 @@ const BabotttomSection = () => {
               </Link>
 
               <Link to="#" className="flex-1 border-black/20">
-                <div className="border-[1px]  flex justify-center items-center h-full xsm:w-[80%] sm:w-full sm:flex-1 hover:text-white hover:bg-orange-600 ">
-                  <div className="flex items-center gap-4">
-                    <GiCruiser className="font-normal xsm:text-[15px] sm:text-[40px] xsm:hidden lg:block" />
-                    <h1 className="font-normal xsm:text-[15px] sm:text-[20px]">
+                <div className="border-[1px]  flex justify-center items-center h-full w-full flex-1 hover:text-white hover:bg-orange-600 ">
+                  <div className="flex items-center lg:gap-4 xsm:gap-2 xsm:p-[5%] sm:m-0 xsm:flex-col sm:flex-row">
+                    <GiCruiser className="font-normal  text-[50px]" />
+                    <h1 className="font-normal  text-[20px]">
                       Cruise
                     </h1>
                   </div>
@@ -120,10 +127,10 @@ const BabotttomSection = () => {
               </Link>
 
               <Link to="#" className="flex-1 border-black/20">
-                <div className="border-[1px]  flex justify-center items-center h-full xsm:w-[80%] sm:w-full sm:flex-1 hover:text-white hover:bg-orange-600 ">
-                  <div className="flex items-center gap-4">
-                    <MdOutlineHolidayVillage className="font-normal xsm:text-[15px] sm:text-[25px] xsm:hidden lg:block" />
-                    <h1 className="font-normal xsm:text-[15px] sm:text-[20px]">
+                <div className="border-[1px]  flex justify-center items-center h-full w-full flex-1 hover:text-white hover:bg-orange-600 ">
+                  <div className="flex items-center lg:gap-4 xsm:gap-2 xsm:p-[5%] sm:m-0 xsm:flex-col sm:flex-row">
+                    <MdOutlineHolidayVillage className="font-normal  text-[30px]" />
+                    <h1 className="font-normal  text-[20px]">
                       Holiday
                     </h1>
                   </div>
@@ -132,17 +139,13 @@ const BabotttomSection = () => {
             </div>
           </div>
 
+          {/* top section ends here   */}
           {/* ...........................................  */}
 
-          {/* upper part section  */}
-
-          {/* lg:flex-row h-auto lg:h-[169px] */}
-          <div
-            className={` flex justify-center items-center xsm:flex-col xsm:items-center w-full lg:flex-row md:px-2 bg-white xsm:h-auto xsm:mb-10 lg:mb-0 md:h-[200px] ${
-              toggle
-                ? "xsm:mt-[200px]  sm:mt-[5px]"
-                : "xsm:mt-[50px]  sm:mt-[5px]"
-            }`}
+          {/* Bottom from section starts here  */}
+          <form
+            onSubmit={(event) => handleHotelBooking(event)}
+            className=" flex justify-center items-center xsm:flex-col xsm:items-center w-full lg:flex-row md:px-2 bg-white xsm:h-auto xsm:mb-10 lg:mb-0 "
           >
             <div className="text-start w-full  px-[2%]">
               <div className="text-xl font-normal text-black flex items-center mb-[14px]">
@@ -150,43 +153,50 @@ const BabotttomSection = () => {
                 <h1>Going to</h1>
               </div>
 
-              {/* <input
-                  type="text"
-                  placeholder="Location here"
-                  className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] w-full lg::w-[280px] text-base font-normal ps-[23px] outline-none"
-                /> */}
-
-              <CountryDropdown className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] w-full lg::w-[280px] text-base font-normal ps-[23px] outline-none text-[#948D8D]"
+              <CountryDropdown
+                className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] w-full lg::w-[280px] text-base font-normal ps-[23px] outline-none text-[#948D8D]"
                 value={country}
                 onChange={(val) => setCountry(val)}
               />
             </div>
 
             <div className="flex justify-between xsm:flex-col sm:flex-row xsm:gap-1  px-[2%] w-full">
-              <div className="text-start flex-1">
-                <div className="text-xl font-normal text-black flex items-center mb-[14px]">
-                  <LuUserCheck />
-                  <h1>Check IN</h1>
+              <div className="flex flex-1 gap-2">
+                <div className="text-start flex-1 gap-2">
+                  <div className="text-xl font-normal text-black flex items-center mb-[14px]">
+                    <LuUserCheck />
+                    <h1>Check IN</h1>
+                  </div>
+
+                  <DatePicker
+                    className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] lg:w-[150px] text-base font-normal outline-none  text-[#948D8D]  overflow-hidden ps-5"
+                    
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    placeholderText="mm/dd/yy"
+                  />
                 </div>
 
-                <input
-                  type="text"
-                  placeholder="dd/mm/yy"
-                  className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] lg:w-[150px] text-base font-normal ps-[23px] outline-none"
-                />
-              </div>
+                <div className="text-start flex-1 ">
+                  <div className="text-xl font-normal text-black flex items-center mb-[14px]">
+                    <LuUserX />
+                    <h1>Check Out</h1>
+                  </div>
 
-              <div className="text-start flex-1">
-                <div className="text-xl font-normal text-black flex items-center mb-[14px]">
-                  <LuUserX />
-                  <h1>Check Out</h1>
+                  <DatePicker
+                    className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] lg:w-[150px] text-base font-normal outline-none text-[#948D8D]  overflow-hidden ps-5"
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    placeholderText="mm/dd/yy"
+                  />
                 </div>
-
-                <input
-                  type="text"
-                  placeholder="dd/mm/yy"
-                  className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] lg:w-[150px] text-base font-normal ps-[23px] outline-none"
-                />
               </div>
 
               <div className="text-start flex-1">
@@ -195,20 +205,37 @@ const BabotttomSection = () => {
                   <h1>Travelers</h1>
                 </div>
 
-                <input
-                  type="number"
-                  placeholder="Travelers"
-                  className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] lg:w-[150px] text-base font-normal ps-[23px] outline-none"
-                />
+                <div className="border-[1px] border-[#948D8D]  rounded-2xl xsm:w-full xsm:h-9 md:h-[45px] lg:w-[150px] text-base font-normal outline-none flex text-[#948D8D] justify-between items-center overflow-hidden">
+                  <div
+                    className=" w-10 h-full text-white text-3xl bg-slate-300 flex justify-center items-center hover:cursor-pointer"
+                    onClick={() => totalPossibletraveler("minus")}
+                  >
+                    -
+                  </div>
+
+                  <div className="flex-1 flex  justify-center">{traveler}</div>
+
+                  <div
+                    className="w-10  h-full text-white text-3xl bg-slate-300 flex justify-center items-center hover:cursor-pointer"
+                    onClick={() => totalPossibletraveler("plus")}
+                  >
+                    +
+                  </div>
+                </div>
               </div>
             </div>
 
             <div className="w-full px-[2%]">
-              <button className="w-full xl:w-[150px] h-[45px] text-white  bg-[#FF3B00] border-[1px] border-gray-400 text-xl font-semibold relative top-5 rounded-2xl">
+              <button
+                className="w-full xl:w-[150px] h-[45px] text-white  bg-[#FF3B00] border-[1px] border-gray-400 text-xl font-semibold relative top-5 rounded-2xl"
+                type="submit"
+              >
                 Search
               </button>
             </div>
-          </div>
+
+          </form>
+          {/* Bottom from section ends here  */}
         </div>
       </div>
     </SectionWrapper>
