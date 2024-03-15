4edc22166870fbdd1caa1858e4f47b1bb5b3a782
Habib Imam <habibrifatx21@gmail.com>
2024-03-15 15:01:44 +0600

    jut trying

--- src/component/BaBookNOw.jsx
+++ src/component/BaBookNOw.jsx
@@ -12,7 +12,7 @@ const BaBookNOw = ({next, previous}) => {
     <div className="w-[100%] flex justify-center xsm:flex-col sm:flex-row sm:justify-between ml-[2.5%] mr-[2.5%]">
       <div className="text-xl font-bold   text-white gap-2">
         <button className="px-[20px] py-[10px] bg-[#FF3B00] rounded-md" onClick={()=>alert("not functional yet")}>
-          Book Now
+          Book ow
         </button>
         <button className="px-[20px] py-[10px] rounded-md" onClick={()=>alert("not functional yet")} >Choose Tour</button>
       </div>
